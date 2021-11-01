59201c8c165b84b4da25c1b524a958a714e06f98
steve wagner <ciroque@live.com>
2021-11-01 14:11:10 -0700

    Initial commit

---
+++ set-up-next-day.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || die "1 argument required, $# provided"
+echo $1 | grep -E -q '^[a-zA-Z0-9-]+$' || die "Directory name argument required, '$1' provided"
+
+DIR_NAME=$1
+
+mkdir $DIR_NAME
+
+cp ./template/template.go $DIR_NAME/solution.go
+
+touch $DIR_NAME/puzzle.txt
+touch $DIR_NAME/example-input.dat
+touch $DIR_NAME/puzzle-input.dat
+
+git add $DIR_NAME/
