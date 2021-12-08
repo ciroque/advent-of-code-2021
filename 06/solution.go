b5f0765da5206bbbca3675191df6fea254b0210e
steve wagner <ciroque@live.com>
2021-12-07 22:23:09 -0800

    Adds files for Day 06

---
+++ 06/solution.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"github.com/ciroque/advent-of-code-2020/support"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"sync"
+	"time"
+)
+
+/*
+	Solution implementation
+*/
+
+/*
+	Main
+*/
+
+type Result struct {
+	answer   int
+	duration int64
+}
+
+func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	waitCount := 3
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(waitCount)
+
+	exampleChannel := make(chan Result)
+	partOneChannel := make(chan Result)
+	partTwoChannel := make(chan Result)
+
+	go doExamples(exampleChannel, &waitGroup)
+	go doPartOne(partOneChannel, &waitGroup)
+	go doPartTwo(partTwoChannel, &waitGroup)
+
+	exampleResult := <-exampleChannel
+	partOneResult := <-partOneChannel
+	partTwoResult := <-partTwoChannel
+
+	waitGroup.Wait()
+
+	log.
+		Info().
+		Int("example-answer", exampleResult.answer).
+		Int64("example-duration", exampleResult.duration).
+		Int("part-one-answer", partOneResult.answer).
+		Int64("part-one-duration", partOneResult.duration).
+		Int("part-two-answer", partTwoResult.answer).
+		Int64("part-two-duration", partTwoResult.duration).
+		Msg("day 04")
+}
+
+/*
+	Executors
+*/
+
+func doExamples(channel chan Result, waitGroup *sync.WaitGroup) {
+	start := time.Now()
+
+	_ = loadPuzzleInput("example-input.dat")
+
+	channel <- Result{
+		answer:   1,
+		duration: time.Since(start).Nanoseconds(),
+	}
+	waitGroup.Done()
+}
+
+func doPartOne(channel chan Result, waitGroup *sync.WaitGroup) {
+	start := time.Now()
+
+	_ = loadPuzzleInput("puzzle-input.dat")
+
+	channel <- Result{
+		answer:   1,
+		duration: time.Since(start).Nanoseconds(),
+	}
+	waitGroup.Done()
+}
+
+func doPartTwo(channel chan Result, waitGroup *sync.WaitGroup) {
+	start := time.Now()
+
+	_ = loadPuzzleInput("puzzle-input.dat")
+
+	channel <- Result{
+		answer:   1,
+		duration: time.Since(start).Nanoseconds(),
+	}
+	waitGroup.Done()
+}
+
+func loadPuzzleInput(filename string) string {
+	return support.ReadFile(filename)
+}
