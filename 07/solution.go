9b5383b5fc44f6b81211509112747aa97271ecc7
steve wagner <ciroque@live.com>
2021-12-09 19:10:09 -0800

    Done with Day 07

--- 07/solution.go
+++ 07/solution.go
@@ -4,6 +4,10 @@ import (
 	"github.com/ciroque/advent-of-code-2020/support"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"math"
+	"sort"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
@@ -11,6 +15,35 @@ import (
 /*
 	Solution implementation
 */
+
+func CalculateLinearFuelConsumption(count int) int {
+	return count
+}
+
+func CalculateTriangularFuelConsumption(count int) int {
+	return ((count + 1) * count) / 2
+}
+
+func FindSolutionForInput(filename string, fuelConsumptionCalculation func(int) int) int {
+	puzzleInput := loadPuzzleInput(filename)
+	sort.Ints(puzzleInput)
+
+	min := puzzleInput[0]
+	max := puzzleInput[len(puzzleInput)-1]
+
+	differences := []int{}
+	for position := min; position < max; position++ {
+		accumulator := 0
+		for _, value := range puzzleInput {
+			accumulator += fuelConsumptionCalculation(int(math.Abs(float64(value - position))))
+		}
+		differences = append(differences, accumulator)
+	}
+
+	sort.Ints(differences)
+
+	return differences[0]
+}
 
 /*
 	Main
@@ -24,19 +57,22 @@ type Result struct {
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
-	waitCount := 3
+	waitCount := 4
 	var waitGroup sync.WaitGroup
 	waitGroup.Add(waitCount)
 
-	exampleChannel := make(chan Result)
+	exampleChannelOne := make(chan Result)
+	exampleChannelTwo := make(chan Result)
 	partOneChannel := make(chan Result)
 	partTwoChannel := make(chan Result)
 
-	go doExamples(exampleChannel, &waitGroup)
+	go doExampleOne(exampleChannelOne, &waitGroup)
+	go doExampleTwo(exampleChannelTwo, &waitGroup)
 	go doPartOne(partOneChannel, &waitGroup)
 	go doPartTwo(partTwoChannel, &waitGroup)
 
-	exampleResult := <-exampleChannel
+	exampleResultOne := <-exampleChannelOne
+	exampleResultTwo := <-exampleChannelTwo
 	partOneResult := <-partOneChannel
 	partTwoResult := <-partTwoChannel
 
@@ -44,8 +80,10 @@ func main() {
 
 	log.
 		Info().
-		Int("example-answer", exampleResult.answer).
-		Int64("example-duration", exampleResult.duration).
+		Int("example-one-answer", exampleResultOne.answer).
+		Int64("example-one-duration", exampleResultOne.duration).
+		Int("example-two-answer", exampleResultTwo.answer).
+		Int64("example-two-duration", exampleResultTwo.duration).
 		Int("part-one-answer", partOneResult.answer).
 		Int64("part-one-duration", partOneResult.duration).
 		Int("part-two-answer", partTwoResult.answer).
@@ -57,13 +95,21 @@ func main() {
 	Executors
 */
 
-func doExamples(channel chan Result, waitGroup *sync.WaitGroup) {
+func doExampleOne(channel chan Result, waitGroup *sync.WaitGroup) {
 	start := time.Now()
 
-	_ = loadPuzzleInput("example-input.dat")
+	channel <- Result{
+		answer:   FindSolutionForInput("example-input.dat", CalculateLinearFuelConsumption),
+		duration: time.Since(start).Nanoseconds(),
+	}
+	waitGroup.Done()
+}
+
+func doExampleTwo(channel chan Result, waitGroup *sync.WaitGroup) {
+	start := time.Now()
 
 	channel <- Result{
-		answer:   1,
+		answer:   FindSolutionForInput("example-input.dat", CalculateTriangularFuelConsumption),
 		duration: time.Since(start).Nanoseconds(),
 	}
 	waitGroup.Done()
@@ -72,10 +118,8 @@ func doExamples(channel chan Result, waitGroup *sync.WaitGroup) {
 func doPartOne(channel chan Result, waitGroup *sync.WaitGroup) {
 	start := time.Now()
 
-	_ = loadPuzzleInput("puzzle-input.dat")
-
 	channel <- Result{
-		answer:   1,
+		answer:   FindSolutionForInput("puzzle-input.dat", CalculateLinearFuelConsumption),
 		duration: time.Since(start).Nanoseconds(),
 	}
 	waitGroup.Done()
@@ -84,15 +128,22 @@ func doPartOne(channel chan Result, waitGroup *sync.WaitGroup) {
 func doPartTwo(channel chan Result, waitGroup *sync.WaitGroup) {
 	start := time.Now()
 
-	_ = loadPuzzleInput("puzzle-input.dat")
-
 	channel <- Result{
-		answer:   1,
+		answer:   FindSolutionForInput("puzzle-input.dat", CalculateTriangularFuelConsumption),
 		duration: time.Since(start).Nanoseconds(),
 	}
 	waitGroup.Done()
 }
 
-func loadPuzzleInput(filename string) string {
-	return support.ReadFile(filename)
+func loadPuzzleInput(filename string) []int {
+	input := support.ReadFile(filename)
+	split := strings.Split(input, ",")
+	var numbers []int
+
+	for _, num := range split {
+		number, _ := strconv.Atoi(num)
+		numbers = append(numbers, number)
+	}
+
+	return numbers
 }
