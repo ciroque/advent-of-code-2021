3337da47dbe2a8846e16f8e02785e557465ec3d0
steve wagner <ciroque@live.com>
2021-12-31 19:29:25 -0800

    Day 14, Part 1

--- 14/solution.go
+++ 14/solution.go
@@ -4,6 +4,7 @@ import (
 	"github.com/ciroque/advent-of-code-2020/support"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"sort"
 	"strings"
 	"sync"
 	"time"
@@ -14,14 +15,17 @@ import (
 */
 
 type PolymerFormulator struct {
-	template       []string
+	elementCounts  []map[string]int
 	insertionRules map[string]string
+	template       string
+	polymers       []string
 }
 
 func NewPolymerFormulator(data []string) PolymerFormulator {
 	polymerFormulator := PolymerFormulator{
-		template:       []string{},
+		elementCounts:  []map[string]int{},
 		insertionRules: map[string]string{},
+		polymers:       []string{},
 	}
 
 	inInsertionRules := false
@@ -35,19 +39,75 @@ func NewPolymerFormulator(data []string) PolymerFormulator {
 			parts := strings.Split(line, " -> ")
 			polymerFormulator.insertionRules[parts[0]] = parts[1]
 		} else {
-			polymerFormulator.template = append(polymerFormulator.template, line)
+			polymerFormulator.template = line
 		}
 	}
 
 	return polymerFormulator
 }
 
+func (pf *PolymerFormulator) GetPairs(polymer string) []string {
+	var windowed []string
+	for index := 0; index < len(polymer)-1; index++ {
+		windowed = append(windowed, polymer[index:index+2])
+	}
+	return windowed
+}
+
+func (pf *PolymerFormulator) RunFirstInsertion() {
+	pf.RunInsertion(pf.template)
+}
+
+func (pf *PolymerFormulator) RunInsertion(polymer string) {
+	var newPolymer []string
+	pairs := pf.GetPairs(polymer)
+
+	for _, pair := range pairs {
+		if insertion, found := pf.insertionRules[pair]; found {
+			newPolymer = append(newPolymer, string(pair[0]), insertion)
+		} else {
+			newPolymer = append(newPolymer, string(pair[0]), string(pair[1]))
+		}
+	}
+
+	newPolymer = append(newPolymer, string(pairs[len(pairs)-1][1]))
+
+	pf.polymers = append(pf.polymers, strings.Join(newPolymer, ""))
+}
+
+func (pf *PolymerFormulator) CalculateSolution() int {
+	counts := []int{}
+	index := len(pf.elementCounts) - 1
+	for _, value := range pf.elementCounts[index] {
+		counts = append(counts, value)
+	}
+
+	sort.Ints(counts)
+
+	return counts[len(counts)-1] - counts[0]
+}
+
+func (pf *PolymerFormulator) CalculateElementFrequencies() {
+	index := len(pf.polymers) - 1
+	elementCounts := map[string]int{}
+	for _, element := range pf.polymers[index] {
+		elementCounts[string(element)]++
+	}
+	pf.elementCounts = append(pf.elementCounts, elementCounts)
+}
+
 func FindSolutionForInput(filename string) int {
-	solution := 0
+	polymerFormulator := NewPolymerFormulator(loadPuzzleInput(filename))
 
-	_ = NewPolymerFormulator(loadPuzzleInput(filename))
+	polymerFormulator.RunFirstInsertion()
+	polymerFormulator.CalculateElementFrequencies()
 
-	return solution
+	for index := 0; index < 9; index++ {
+		polymerFormulator.RunInsertion(polymerFormulator.polymers[index])
+		polymerFormulator.CalculateElementFrequencies()
+	}
+
+	return polymerFormulator.CalculateSolution()
 }
 
 /*
@@ -124,7 +184,7 @@ func doPartOne(channel chan Result, waitGroup *sync.WaitGroup) {
 	start := time.Now()
 
 	channel <- Result{
-		answer:   0, //FindSolutionForInput("puzzle-input.dat"),
+		answer:   FindSolutionForInput("puzzle-input.dat"),
 		duration: time.Since(start).Nanoseconds(),
 	}
 	waitGroup.Done()
