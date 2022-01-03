48920315a2a41f37b5118d86748cef19dda5acd7
steve wagner <ciroque@live.com>
2022-01-02 18:54:23 -0800

    Adds Day 15 files

---
+++ 15/solution.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"github.com/ciroque/advent-of-code-2020/support"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"sync"
+	"time"
+)
+
+/*
+	Solution implementation
+*/
+
+func FindSolutionForInput(filename string) int {
+	solution := 0
+
+	return solution
+}
+
+/*
+	Main
+*/
+
+type Result struct {
+	answer   int
+	duration int64
+}
+
+func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	waitCount := 4
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(waitCount)
+
+	exampleChannelOne := make(chan Result)
+	exampleChannelTwo := make(chan Result)
+	partOneChannel := make(chan Result)
+	partTwoChannel := make(chan Result)
+
+	go doExampleOne(exampleChannelOne, &waitGroup)
+	go doExampleTwo(exampleChannelTwo, &waitGroup)
+	go doPartOne(partOneChannel, &waitGroup)
+	go doPartTwo(partTwoChannel, &waitGroup)
+
+	exampleResultOne := <-exampleChannelOne
+	exampleResultTwo := <-exampleChannelTwo
+	partOneResult := <-partOneChannel
+	partTwoResult := <-partTwoChannel
+
+	waitGroup.Wait()
+
+	log.
+		Info().
+		Int("example-one-answer", exampleResultOne.answer).
+		Int64("example-one-duration", exampleResultOne.duration).
+		Int("example-two-answer", exampleResultTwo.answer).
+		Int64("example-two-duration", exampleResultTwo.duration).
+		Int("part-one-answer", partOneResult.answer).
+		Int64("part-one-duration", partOneResult.duration).
+		Int("part-two-answer", partTwoResult.answer).
+		Int64("part-two-duration", partTwoResult.duration).
+		Msg("Solved!")
+}
+
+/*
+	Executors
+*/
+
+func doExampleOne(channel chan Result, waitGroup *sync.WaitGroup) {
+	start := time.Now()
+
+	channel <- Result{
+		answer:   FindSolutionForInput("example-input.dat"),
+		duration: time.Since(start).Nanoseconds(),
+	}
+	waitGroup.Done()
+}
+
+func doExampleTwo(channel chan Result, waitGroup *sync.WaitGroup) {
+	start := time.Now()
+
+	channel <- Result{
+		answer:   FindSolutionForInput("example-input.dat"),
+		duration: time.Since(start).Nanoseconds(),
+	}
+	waitGroup.Done()
+}
+
+func doPartOne(channel chan Result, waitGroup *sync.WaitGroup) {
+	start := time.Now()
+
+	channel <- Result{
+		answer:   FindSolutionForInput("puzzle-input.dat"),
+		duration: time.Since(start).Nanoseconds(),
+	}
+	waitGroup.Done()
+}
+
+func doPartTwo(channel chan Result, waitGroup *sync.WaitGroup) {
+	start := time.Now()
+
+	channel <- Result{
+		answer:   FindSolutionForInput("puzzle-input.dat"),
+		duration: time.Since(start).Nanoseconds(),
+	}
+	waitGroup.Done()
+}
+
+func loadPuzzleInput(filename string) string {
+	return support.ReadFile(filename)
+}
